5b56b470ad518c1356a347d055c18d2972e731a6
r0mka <romanliveshere@gmail.com>
2021-03-25 22:22:11 -0700

    Initial Commit

---
+++ cherno-c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cherno-c++
+//
+//  Created by Roman Nikolaenkov on 3/25/21.
+//  Copyright © 2021 Roman Nikolaenkov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
